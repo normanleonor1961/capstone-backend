2ccb75645a059731d68c144d44ad148fd6b4cda1
normanleonor1961 <109060058+normanleonor1961@users.noreply.github.com>
2022-10-06 15:06:55 +0800

    Add SchemaConfiguration

---
+++ src/main/java/norman/minicapstone/config/SchemaConfiguration.java
@@ -0,0 +1,10 @@
+package norman.minicapstone.config;
+
+public class SchemaConfiguration {
+
+    public static final String SCHEMA_NAME = "capstone";
+
+    private SchemaConfiguration() {
+        throw new AssertionError("Never instantiate");
+    }
+}
